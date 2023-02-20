3c64c2e83b1ec1a70438625265bef76c92a7a2ce
Harry <107435091+hazzaaclark@users.noreply.github.com>
2023-02-20 01:57:57 +0000

    Add files via upload

---
+++ include/file/file.h
@@ -0,0 +1,23 @@
+/* Copyright (C) 2023 Harry Clark */
+
+/* Pure3D DLL Library */
+
+/* THIS FILE PERTAINS TO THE FILE MANAGEMENT INFRASTRUCTURE */
+/* FOR PARSING PURE3D'S PREPRIATORY FILE FORMAT */
+
+#ifndef PURE_3D_FILE
+#define PURE_3D_FILE
+
+/* SYSTEM INCLUDES */
+
+#include <map>
+#include <memory>
+#include <string>
+
+#ifndef FILE_STRUCTS
+#define FILE_STRUCTS
+
+
+#endif
+
+#endif
